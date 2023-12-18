fde8331f9ed10597f899b2db0c23ca128f0af49a
Muhamad Royhan Fadhli <122357726+MRoyhanF@users.noreply.github.com>
2023-12-18 10:52:51 +0700

    done lear jwt

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import db from "./config/Database.js"
 import router from "./routes/index.js";
 // import Users from "./models/UserModel.js";
@@ -14,6 +16,8 @@ try {
     console.log(error);
 }
 
+ap.use(cors({credential: true, origin:'http://localhost:3000'}));
+app.use(cookieParser());
 app.use(express.json());
 app.use(router);
 
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 import express from "express";
-import {getUser, Register, Login} from "../controller/Users.js"
+import {getUser, Register, Login, Logout} from "../controller/Users.js"
 import { verifyToken } from "../middleware/VerifyToken.js";
+import { refershToken } from "../controller/RefreshToken.js";
 
 const router = express.Router();
 
 router.get('/users', verifyToken, getUser);
 router.post('/users', Register);
 router.post('/login', Login);
+router.get('/token', refershToken);
+router.delete('/Logout', Logout);
 
 export default router;
